c60ba9ac0d8c447e7ccbecaf2559b9d73d752f63
Álvaro Puente <alvaropuentecorrea@gmail.com>
2024-07-18 00:07:09 +0200

    Added csv file for logs, Thursday test

--- script/server_status.py
+++ script/server_status.py
@@ -1,10 +1,13 @@
-import smtplib, ssl
-import subprocess
-from email.mime.text import MIMEText
-import time
-import pytz
 from datetime import datetime
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import csv
 import os
+import pytz
+import smtplib
+import subprocess
 
 SMTP_HOST = os.environ.get('SMTP_HOST')
 SMTP_PORT = os.environ.get('SMTP_PORT')
@@ -15,19 +18,31 @@ TO_EMAIL = os.environ.get('TO_EMAIL')
 SERVER_HOST = os.environ.get('SERVER_HOST')
 
 def send_email(subject, body):
-    msg = MIMEText(body)
+    msg = MIMEMultipart()
     msg['Subject'] = subject
     msg['From'] = FROM_EMAIL
     msg['To'] = TO_EMAIL
+
+    # Adjuntar el archivo CSV
+    with open('server_status.csv', 'rb') as csvfile:
+        attachment = MIMEBase('application', 'octet-stream')
+        attachment.set_payload(csvfile.read())
+        encoders.encode_base64(attachment)
+        attachment.add_header('Content-Disposition', 'attachment', filename='server_status.csv')
+        msg.attach(attachment)
+
+    # Adjuntar el cuerpo del mensaje
+    msg.attach(MIMEText(body, 'plain'))
 
     with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as smtp:
         smtp.starttls()
         smtp.login(SMTP_USERNAME, SMTP_PASSWORD)
         smtp.send_message(msg)
 
+
 def ping_server(host):
     try:
-        print(subprocess.check_output(['curl', host], universal_newlines=True))
+        subprocess.check_output(['curl', host], universal_newlines=True)
         return True
     except subprocess.CalledProcessError:
         return False
@@ -36,14 +51,22 @@ def ping_server(host):
 TZ_MADRID = pytz.timezone('Europe/Madrid')
 CURRENT_TIME = datetime.now(TZ_MADRID).strftime('%H:%M:%S')
 CURRENT_HOUR = datetime.now(TZ_MADRID).strftime('%H')
+CURRENT_DAY = datetime.now().strftime('%A')
+
 
 if ping_server(SERVER_HOST):
-    if CURRENT_HOUR == '06':
+    if CURRENT_DAY == 'Thursday' and CURRENT_HOUR == '00':
+        with open('server_status.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([CURRENT_TIME, CURRENT_DAY, SERVER_HOST, 'Server ist aktiv'])
         send_email('Server Status', f'{CURRENT_TIME} Server {SERVER_HOST} ist aktiv.')
-        print(f'{CURRENT_TIME} SERVER AKTIV')
     else:
-        print(f'{CURRENT_TIME} Server ist aktiv')
+        with open('server_status.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([CURRENT_TIME, CURRENT_DAY, SERVER_HOST, 'Server ist aktiv'])
 else:
+    with open('server_status.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([CURRENT_TIME, CURRENT_DAY, SERVER_HOST, 'ANTWORTET NICHT'])
     send_email('Achtung: Server Down Alarm', f'{CURRENT_TIME} Server {SERVER_HOST} antwortet nicht.')
-    print(f'{CURRENT_TIME} SERVER ANTWORTET NICHT')
 
